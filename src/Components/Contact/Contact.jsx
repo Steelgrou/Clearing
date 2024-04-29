8b31abba0e15fd08d87f39a8078d84ac1b47687a
Steelgrou <steelgrou03@gmail.com>
2024-04-30 00:45:17 +0500

    E

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -57,28 +57,45 @@ export default function Contact() {
                                     <a href="tel:+998993024512">(414) 567 - 2109</a>
 
                                 </div>
-                                <hr />
+
                             </div>
-                            <h5>{t('contact-quest')}</h5>
-                            <h6>{t('contact-desc')}</h6>
-                            <button>{t('contact-btn')}</button>
+                            <h5 className='contact-quest'>{t('contact-quest')}</h5>
+                            <h6 className='contact-desc'>{t('contact-desc')}</h6>
+                            <button className='contact-btn'>{t('contact-btn')}</button>
                         </div>
                         <div className="contact-right">
                             <form id='myForm' onSubmit={sendMessage}>
-                                <label htmlFor="text">{t('contact-fullname')}</label>
-                                <input type="text" id='name' required />
-                                <label htmlFor="">{t('contact-phone')}</label>
-                                <input type="text" id='number' placeholder='+998' required />
-                                <label htmlFor="">{t('contact-address')}</label>
-                                <input type="text" id='adress' placeholder={t('contact-holder')} required />
-                                <label htmlFor="">{t('contact-email')}</label>
-                                <input type="text" id='email' placeholder='......@gmail.com' required />
-                                <label htmlFor=""> {t('contact-request')}</label>
-                                <input type="text" id='service' />
-                                <label htmlFor="">{t('contact-service')}</label>
-                                <input type="text" id='day' />
-                                <label htmlFor="">{t('contact-add')}</label>
-                                <input type="text" id='add' />
+
+                                <div className="div">
+                                    <label htmlFor="text">{t('contact-fullname')}</label>
+                                    <input type="text" id='name' required />
+                                </div>
+
+                                <div className="div">
+                                    <label htmlFor="">{t('contact-phone')}</label>
+                                    <input type="text" id='number' placeholder='+998' required />
+                                </div>
+
+                                <div className="div">
+                                    <label htmlFor="">{t('contact-address')}</label>
+                                    <input type="text" id='adress' placeholder={t('contact-placeholder-address')} required />
+                                </div>
+                                <div className="div">
+                                    <label htmlFor="">{t('contact-email')}</label>
+                                    <input type="text" id='email' placeholder={t('contact-placeholder-email')} required />
+                                </div>
+                                <div className="div">
+                                    <label htmlFor=""> {t('contact-request')}</label>
+                                    <input type="text" id='service' />
+                                </div>
+                                <div className="div">
+                                    <label htmlFor="">{t('contact-service')}</label>
+                                    <input type="text" id='day' />
+                                </div>
+                                <div className="btn">
+                                    <label htmlFor="">{t('contact-add')}</label>
+                                    <input type="text" id='add' />
+                                </div>
                                 <button className='btn-grad' type='submit'>{t('contact-submit')}</button>
                             </form>
 
